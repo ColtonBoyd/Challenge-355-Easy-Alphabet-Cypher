fe1d69e22b94e49e949558ff3fa990518b4f8e7b
Colton C <colto42@gmail.com>
2018-10-22 17:09:14 -0400

    Completed First part

--- Challenge 355 Easy Alphabet Cypher/Program.cs
+++ Challenge 355 Easy Alphabet Cypher/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Challenge_355_Easy_Alphabet_Cypher
 {
@@ -10,6 +8,35 @@ namespace Challenge_355_Easy_Alphabet_Cypher
     {
         static void Main(string[] args)
         {
+            var x = File.ReadAllLines("Input.txt");
+            
+            foreach (var item in x)
+            {
+                string Encryption = item.Substring(0, item.IndexOf(" "));
+                string Code = item.Substring(item.IndexOf(" ") + 1);
+                string Mess = "";
+                string LongEncryption = "";
+
+                for (int i = 0; i < Code.Count(); i++)
+                {
+                    LongEncryption += Encryption[i % Encryption.Count()];
+                }
+
+                for (int i = 0; i < LongEncryption.Length; i++)
+                {
+                    int Location = (LongEncryption[i] - 97) + Code[i];
+                    if (Location > 122)
+                        Location -= 26;
+                    Mess += (char)Location;
+
+                }
+                //System.Console.WriteLine("Original Phrase : {0} \t Cypher : {1} \t Encrypted Message{2}", Code, Encryption, Mess);
+
+                Console.WriteLine(Mess);
+                
+            }
+
+            Console.ReadLine();
         }
     }
 }
